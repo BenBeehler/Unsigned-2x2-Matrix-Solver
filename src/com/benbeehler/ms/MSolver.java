d45fddec5f2ddf6a454d7742a0f90b42b7de0dbb
iDeveloper <ben.beehler@gmail.com>
2017-12-06 22:46:52 -0500

    Updated Program Safety
    added exception handling and removed unnecessary code bits

--- src/com/benbeehler/ms/MSolver.java
+++ src/com/benbeehler/ms/MSolver.java
@@ -1,15 +1,12 @@
 package com.benbeehler.ms;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class MSolver {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-       	System.out.println("please enter the coordinates of each line"); 
-		long start = System.nanoTime();
-
+       	System.out.println("Please enter the coefficients/constants of these two lines"); 
 		double tL = sc.nextDouble();
 		
 		System.out.println("Line #1 x coefficient = " + tL);
@@ -27,13 +24,14 @@ public class MSolver {
 		System.out.println("Line #2 constant = " + yC);
 		sc.close();
 		double[] values = new double[] { tL, bL, tR, bR, xC, yC };
+		long start = System.nanoTime();
 		init(values);
 		
 		Long end = System.nanoTime();
 		Long time = (end - start);
 		
 		System.out.println();
-		System.out.println("Took " + (time / 1000000.0) + " milli seconds to solve matrix");
+		System.out.println("Took " + (time / 1000000.0) + " milli seconds to solve the problem");
 	}
 	
 	public static void init(double[] values) {
@@ -82,34 +80,39 @@ public class MSolver {
 		 * bR should = 1
 		 */
 		
-		double tL = values[0];
-		double bL = values[1];
-		double tR = values[2];
-		double bR = values[3];
-		double xC = values[4];
-		double yC = values[5];
-		
-		System.out.println("{" + tL + "    " + tR + "   |    " + xC + "}");
-		System.out.println("{" + bL + "    " + bR + "   |    " + yC + "}");
-		System.out.println();
-		
-		double newbL = (tL*bL) - (bL*tL);
-		double newbR = (tR*bL) - (bR*tL);
-		double newyC = (xC*bL) - (yC*tL);
-		
-		System.out.println("{" + tL + "    " + tR + "   |    " + xC + "}");
-		System.out.println("{" + newbL + "    " + newbR + "   |    " + newyC + "}");
-		System.out.println();
-		
-		double newtR = (tR * newbR) - (newbR * tR);
-		double newtL = (tL * newbR) - (newbL * tR);
-		double newxC = (xC * newbR) - (newyC * tR);
-		
-		System.out.println("{" + newtL + "    " + newtR + "   |    " + newxC + "}");
-		System.out.println("{" + newbL + "    " + newbR + "   |    " + newyC + "}");
-		System.out.println();
-		
-		values = new double[] { newtL, newbL, newtR, newbR, newxC, newyC };
+		try {
+			double tL = values[0];
+			double bL = values[1];
+			double tR = values[2];
+			double bR = values[3];
+			double xC = values[4];
+			double yC = values[5];
+			
+			System.out.println("{" + tL + "    " + tR + "   |    " + xC + "}");
+			System.out.println("{" + bL + "    " + bR + "   |    " + yC + "}");
+			System.out.println();
+			
+			double newbL = (tL*bL) - (bL*tL);
+			double newbR = (tR*bL) - (bR*tL);
+			double newyC = (xC*bL) - (yC*tL);
+			
+			System.out.println("{" + tL + "    " + tR + "   |    " + xC + "}");
+			System.out.println("{" + newbL + "    " + newbR + "   |    " + newyC + "}");
+			System.out.println();
+			
+			double newtR = (tR * newbR) - (newbR * tR);
+			double newtL = (tL * newbR) - (newbL * tR);
+			double newxC = (xC * newbR) - (newyC * tR);
+			
+			System.out.println("{" + newtL + "    " + newtR + "   |    " + newxC + "}");
+			System.out.println("{" + newbL + "    " + newbR + "   |    " + newyC + "}");
+			System.out.println();
+			
+			values = new double[] { newtL, newbL, newtR, newbR, newxC, newyC };
+		} catch(ArithmeticException e) {
+			System.out.println("Error => It is safe to assume the given values cannot produce a logical output");
+			System.exit(1);
+		}
 		
 		return values;
 	}
